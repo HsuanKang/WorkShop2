0ed0dca8754e3cf0bf7e2a0eb08f43de89d63b4b
HsuanKang <ting985477@gmail.com>
2019-06-07 12:19:57 +0800

    v3

--- WorkShop2/Controllers/LibraryController.cs
+++ WorkShop2/Controllers/LibraryController.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace WorkShop2.Controllers
 {
@@ -22,11 +23,32 @@ namespace WorkShop2.Controllers
             SqlConnection conn = new SqlConnection(DBstr());
             String sql = "Select * From [dbo].[BOOK_DATA]";
             SqlCommand scm = new SqlCommand(sql, conn);
-            SqlDataAdapter adapter = new SqlDataAdapter(scm);
+            SqlDataAdapter adapter = new SqlDataAdapter(scm); //連接db
             DataSet dataSet = new DataSet();
             adapter.Fill(dataSet, "data");
-            data = dataSet.Tables[0];
+
+            String json = GetJson(dataSet);
+            ViewBag.jstr = json;
             return View(dataSet);
+        }
+
+        public string GetJson(DataSet ds) //轉成Json
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new
+
+            System.Web.Script.Serialization.JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row;
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in ds.Tables[0].Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+            return serializer.Serialize(rows);
         }
     }
 }
